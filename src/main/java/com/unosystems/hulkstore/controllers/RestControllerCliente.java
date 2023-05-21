7b14078005a26f563cc2fc470306f1f46cc9703e
Juan Pablo <aetosyaoista97@gmail.com>
2023-05-21 02:08:50 -0500

    se crea los endpoints para actualizar cliente y eliminar cliente

--- src/main/java/com/unosystems/hulkstore/controllers/RestControllerCliente.java
+++ src/main/java/com/unosystems/hulkstore/controllers/RestControllerCliente.java
@@ -15,7 +15,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
-import java.util.zip.DataFormatException;
+
 
 @org.springframework.web.bind.annotation.RestController
 @RequestMapping("/api")
@@ -33,7 +33,7 @@ public class RestControllerCliente {
     //Mostrar por id de cliente
     @GetMapping("/cliente/{id}")
     public ResponseEntity<?> mostrarId(@PathVariable Long id){
-        Cliente cliente = null;
+        Cliente cliente;
         Map<String,Object> response = new HashMap<>();
         try{
             cliente = clienteService.findById(id);
@@ -57,7 +57,7 @@ public class RestControllerCliente {
     @PostMapping("/clientes")
     public ResponseEntity<?> guardarCliente(@Valid @RequestBody Cliente cliente, BindingResult result){
 
-        Cliente clienteNuevo = null;
+        Cliente clienteNuevo;
         Map<String, Object> response = new HashMap<>();
 
         if(result.hasErrors()){
@@ -82,6 +82,73 @@ public class RestControllerCliente {
         return new ResponseEntity<>(response, HttpStatus.CREATED);
 
     }
+
+    //actualizar cliente
+    @PutMapping("/cliente/{id}")
+    public ResponseEntity<?> actualizar(@Valid @RequestBody Cliente cliente, @PathVariable Long id,BindingResult result){
+        Cliente clienteActual = clienteService.findById(id);
+        Cliente clienteActualizado;
+
+        Map<String,Object> response = new HashMap<>();// lugar donde ira lso mensajes de exito o error, tambien nuestra respuesta
+        if(result.hasErrors()){
+
+            List<String> errors = result.getFieldErrors()//forma con streams para capturar errores
+                    .stream()
+                    .map(err -> "El campo '"+err.getField()+"' "+err.getDefaultMessage())
+                    .collect(Collectors.toList());
+
+            response.put("error", errors);
+            return new ResponseEntity<>(response,HttpStatus.BAD_REQUEST);
+        }
+        //si el id del cliente es nulo
+        if( clienteActual == null){
+            response.put("mensaje","Error: no se pudo editar, el cliente Id: ".concat(id.toString()).concat(" no existe en la base de datos"));
+            return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        try{
+            clienteActual.setCedula(cliente.getCedula());
+            clienteActual.setNombre(cliente.getNombre());
+            clienteActual.setApellido(cliente.getApellido());
+            clienteActual.setEmail(cliente.getEmail());
+            clienteActual.setCreateAt(cliente.getCreateAt());
+
+            clienteActualizado = clienteService.save(clienteActual);
+
+        }catch(DataAccessException e){
+            response.put("mensaje","Error al actualizar el cliente en la base de datos");
+            response.put("error",e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
+            return new ResponseEntity<>(response,HttpStatus.INTERNAL_SERVER_ERROR);
+
+        }
+        response.put("mensaje","El Cliente ha sido actualizado");
+        response.put("cliente", clienteActualizado);
+
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
+
+    //eliminar cliente
+    @DeleteMapping("/clientes/{id}")
+    public ResponseEntity<?> eliminar(@PathVariable Long id){
+
+        Map<String,Object> response = new HashMap<>();
+
+        try{
+
+            clienteService.delete(id);
+
+        }catch(DataAccessException e){
+            response.put("mensaje","Error al eliminar el cliente en la base de datos");
+            response.put("error",e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
+            return new ResponseEntity<>(response,HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        response.put("mensaje", "El cliente ha sido eliminado");
+        return new ResponseEntity<>(response, HttpStatus.OK);
+
+    }
+
+
+
 
 
 }
